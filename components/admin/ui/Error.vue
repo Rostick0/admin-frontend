c8b8bac7335ce46b406eed889e67ea53d3f807e8
unknown <rostik057@gmail.com>
2024-02-18 00:32:13 +0300

    init

---
+++ components/admin/ui/Error.vue
@@ -0,0 +1,14 @@
+<template>
+  <span class="error">
+    <slot></slot>
+  </span>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.error {
+  color: rgb(var(--color-red));
+  font-size: 0.85rem;
+}
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- {{ res }} -->
+  <AdminLayout>
+    
+  </AdminLayout>
+</template>
+
+<script setup>
+// const res = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=5');
+
+// const data = await res?.json();
+// console.log(data);
+
+</script>
+
+<style lang="scss">
+
+</style>
